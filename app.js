2ce74dad36cd1ad9a891c7764c9a23e59ba6640d
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-06-26 17:56:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  console.log('33');
+});
+
+// 
+
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const track = document.querySelector(".track");
+
+const NUM_SLIDES = 3;
+
+let currSlide = 0;
+
+function slideTrack() {
+  track.style.transform = `translateX(${-currSlide * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  currSlide = (currSlide + 1) % NUM_SLIDES;
+  slideTrack();
+});
+
+prevBtn.addEventListener("click", () => {
+  currSlide = (currSlide - 1 + NUM_SLIDES) % NUM_SLIDES;
+  slideTrack();
+});
